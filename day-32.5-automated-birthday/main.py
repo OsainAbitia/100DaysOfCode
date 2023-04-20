fd1c9560db112d15ea39de5389908e205cc17679
Osain Abitia <osain.abitia@gmail.com>
2023-04-19 23:57:39 -0600

    Remove unused import

--- day-32.5-automated-birthday/main.py
+++ day-32.5-automated-birthday/main.py
@@ -1,8 +1,6 @@
 """Entrypoint file to send Email using smtplib module"""
 from email_sender import EmailSender
 
-from scheduler import Scheduler
-
 if __name__ == "__main__":
     """Entrypoint function to send Email"""
     EmailSender().check_birthdays()
